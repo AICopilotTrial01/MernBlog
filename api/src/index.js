03d1106ba4e0e93523d9b61ea23be7a425396694
wayan darma <wayandarma2010@gmail.com>
2024-03-16 00:47:23 +0800

    moving the node module, from api to root, and adding .env file, also with dotenv dev, that we can process the env file

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,19 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config();
+console.log(process.env.MONGODB_URI);
+mongoose
+  .connect(process.env.MONGODB)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Failed to connect to MongoDB", err);
+  });
 
 const app = express();
-const PORT = 3000;
+const PORT = 8888;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
