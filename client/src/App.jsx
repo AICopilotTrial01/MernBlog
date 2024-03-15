56f111dcbd052a7b5d633f69fa533c3f4b84aa36
wayan darma <wayandarma2010@gmail.com>
2024-03-15 22:54:26 +0800

    test 1, push the code to repo

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import "./index.css";
+function App() {
+  return (
+    <div>
+      <h1 className="text-4xl text-center text-blue-500">Hello World</h1>
+    </div>
+  );
+}
+
+export default App;
