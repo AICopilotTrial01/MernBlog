46a0b5a56974b7ae7ed4fef3377caaa0892274dd
wayan darma <wayandarma2010@gmail.com>
2024-03-15 23:09:59 +0800

    adding the user routes in APP.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,27 @@
 import "./index.css";
+
+// import all the page components
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Dashboard from "./pages/Dashboard";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import Project from "./pages/Project";
+
+// adding the routes using react-router-dom
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <div>
-      <h1 className="text-4xl text-center text-blue-500">Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/signUp" element={<SignUp />} />
+        <Route path="/project" element={<Project />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
