bf6768734b2a91a0368d75a33595f18ea1a54274
wayan darma <wayandarma2010@gmail.com>
2024-03-18 15:51:02 +0800

    adding post method to backend server, and set the proxy that targeted the /api endpoint, with localhost hosting

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,46 @@
 import { Button, Label, TextInput } from "flowbite-react";
 import { Link } from "react-router-dom";
-
+import { useState } from "react";
 function SignUp() {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (
+      formData.username === "" ||
+      formData.email === "" ||
+      formData.password === ""
+    ) {
+      alert("Please fill all the fields");
+      return;
+    }
+    try {
+      const response = await fetch("/api/auth/sign-up", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await response.json();
+      console.log(data);
+    } catch (err) {
+      console.log(err);
+    }
+    console.log(formData);
+  };
+
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value });
+    console.log(formData);
+  };
   return (
     <div className="min-h-screen mt-20">
-      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row gap-4 md:items-center">
+      <div className="flex p-3 max-w-6xl mx-auto flex-col md:flex-row gap-10 md:items-center">
         <div className="flex-1" id="left-side">
           <h1 className=" self-center whitespace-nowrap text-3xl md:text-4xl font-bold dark:text-white">
             <span className=" px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white ">
@@ -19,18 +55,37 @@ function SignUp() {
           </p>
         </div>
         <div className="flex-1" id="right-side">
-          <form className="flex flex-col gap-4">
+          <form
+            className="flex flex-col gap-4"
+            onSubmit={handleSubmit}
+            method="post"
+          >
             <div>
               <Label className="mb-2" value="Your Username" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                onChange={handleInputChange}
+              />
             </div>
             <div>
               <Label className="mb-2" value="Your Email" />
-              <TextInput type="text" placeholder="Email" id="email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleInputChange}
+              />
             </div>
             <div>
               <Label className="mb-2" value="Your Password" />
-              <TextInput type="text" placeholder="Password" id="password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleInputChange}
+              />
             </div>
             <Button
               gradientDuoTone="purpleToPink"
