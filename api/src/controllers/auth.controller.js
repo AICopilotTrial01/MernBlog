5e91f39fef11f41a82f58eb79a08a507e6ac52f1
wayan darma <wayandarma2010@gmail.com>
2024-03-19 11:37:43 +0800

    working with server, adding signin API on the server

--- api/src/controllers/auth.controller.js
+++ api/src/controllers/auth.controller.js
@@ -2,7 +2,8 @@ import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 // importing the bcryptjs package
 import bcryptjs from "bcryptjs";
-
+import jwt from "jsonwebtoken";
+// json web token --> are used to secure the data that is being sent between the client and the server
 export const signUp = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -33,12 +34,40 @@ export const signUp = async (req, res, next) => {
   }
 };
 
-export const signIn = async (req, res) => {
+export const signIn = async (req, res, next) => {
   const { email, password } = req.body;
-  res.json({
-    user: {
-      email,
+
+  if (!email || !password || email === "" || password === "") {
+    return next(errorHandler(400, "Please fill in all the fields correctly"));
+  }
+
+  try {
+    // check the email of the user
+    const validUser = await User.findOne({ email: email });
+    // if the email is not found and than return an error message
+    if (!validUser) {
+      return next(errorHandler(400, "Invalid email"));
+    }
+    // if the emails is found than compare the password)
+    const validPassword = await bcryptjs.compareSync(
       password,
-    },
-  });
+      validUser.password
+    );
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid password"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    // remove the password from the user object
+    const { password: passwordPass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (err) {
+    next(err);
+  }
 };
--- api/src/routes/auth.routes.js
+++ api/src/routes/auth.routes.js
@@ -4,5 +4,5 @@ import { signUp, signIn } from "../controllers/auth.controller.js";
 const router = express.Router();
 
 router.post("/sign-up", signUp);
-router.post("/login", signIn);
+router.post("/sign-in", signIn);
 export default router;
